53637c38d1f95ec2d6c94bc74cef1f7b0389b445
Jonahnki <johnnaadedeji2018@gmail.com>
2025-08-14 18:03:50 +0100

    Add phylogenetic, search, dashboard, and map pages; update layout and GlobalMap; add screenshots

--- src/components/maps/GlobalMap.tsx
+++ src/components/maps/GlobalMap.tsx
@@ -1,19 +1,42 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+interface Outbreak {
+  id: string;
+  name: string;
+  lat: number;
+  lng: number;
+}
 
 interface GlobalMapProps {
-  outbreaks: any[];
+  outbreaks: Outbreak[];
 }
+
+// Override default icons
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: '/leaflet/images/marker-icon-2x.png',
+  iconUrl: '/leaflet/images/marker-icon.png',
+  shadowUrl: '/leaflet/images/marker-shadow.png',
+});
 
 const GlobalMap: React.FC<GlobalMapProps> = ({ outbreaks }) => {
   return (
-    <div>
-      {outbreaks.map((_outbreak, index) => (
-        <div key={index} className="map-marker">
-          {/* Marker rendering */}
-        </div>
+    <MapContainer center={[0, 0]} zoom={2} style={{ height: '600px', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+      {outbreaks.map((ob) => (
+        <Marker key={ob.id} position={[ob.lat, ob.lng]}>
+          <Popup>{ob.name}</Popup>
+        </Marker>
       ))}
-    </div>
+    </MapContainer>
   );
 };
 
 export default GlobalMap;
+
